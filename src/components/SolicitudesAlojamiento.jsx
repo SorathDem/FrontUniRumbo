31cb9654b321d8ef8122b539ed49512fa2b461ff
Sorath <santiagoacostaavila2905@gmail.com>
2025-11-12 23:42:55 -0500

    src-6

--- src/components/SolicitudesAlojamiento.jsx
+++ src/components/SolicitudesAlojamiento.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "../styles/Solicitudes.css";
+import "../styles/solicitudes.css";
 import HeaderArrendatario from "./HeaderArrendatario";
 
 export default function SolicitudesAlojamiento() {
--- src/components/SolicitudesRuta.jsx
+++ src/components/SolicitudesRuta.jsx
@@ -1,7 +1,7 @@
 // src/components/SolicitudesRuta.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/Solicitudes.css";
+import "../styles/solicitudes.css";
 import HeaderConductor from "./HeaderConductor";
 
 export default function SolicitudesRuta() {
