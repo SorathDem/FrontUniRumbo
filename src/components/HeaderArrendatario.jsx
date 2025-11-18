b747fd36227e6bf905ebda1703e20eb3cb95ffcb
1820004 <165920547+1820004@users.noreply.github.com>
2025-11-17 19:23:47 -0500

    Add files via upload

--- src/components/HeaderArrendatario.jsx
+++ src/components/HeaderArrendatario.jsx
@@ -32,51 +32,54 @@ const HeaderArrendatario = () => {
       />
 
       <header className="main-header">
-        {/* IZQUIERDA: logo + título */}
-        <div className="header-left">
-          <div className="logo-block">
-            <img src={logoUdec} alt="Logo UDec" className="logo-udec" />
-            <h1 className="header-title">UniRumbo - Arrendador</h1>
+        {/* Contenedor interno centrado */}
+        <div className="main-header-inner">
+          {/* IZQUIERDA: logo + título */}
+          <div className="header-left">
+            <div className="logo-block">
+              <img src={logoUdec} alt="Logo UDec" className="logo-udec" />
+              <h1 className="header-title">UniRumbo - Arrendador</h1>
+            </div>
           </div>
-        </div>
 
-        {/* CENTRO: navegación */}
-        <nav className="header-center">
-          <Link to="/usuarioArrendatario" className="nav-item">
-            <FaUser className="nav-icon" />
-            <span>Usuario</span>
-          </Link>
+          {/* CENTRO: navegación */}
+          <nav className="header-center">
+            <Link to="/usuarioArrendatario" className="nav-item">
+              <FaUser className="nav-icon" />
+              <span>Usuario</span>
+            </Link>
 
-          <Link to="/Alojamientos" className="nav-item">
-            <FaBed className="nav-icon" />
-            <span>Mis Alojamientos</span>
-          </Link>
+            <Link to="/Alojamientos" className="nav-item">
+              <FaBed className="nav-icon" />
+              <span>Mis Alojamientos</span>
+            </Link>
 
-          {idUsuario && (
-            <>
-              <Link to={`/servicios/${idUsuario}`} className="nav-item">
-                <FaHandshake className="nav-icon" />
-                <span>Servicios</span>
-              </Link>
+            {idUsuario && (
+              <>
+                <Link to={`/servicios/${idUsuario}`} className="nav-item">
+                  <FaHandshake className="nav-icon" />
+                  <span>Servicios</span>
+                </Link>
 
-              <Link
-                to={`/solicitudes-alojamiento/${idUsuario}`}
-                className="nav-item"
-              >
-                <FaHome className="nav-icon" />
-                <span>Solicitudes</span>
-              </Link>
-            </>
-          )}
-        </nav>
+                <Link
+                  to={`/solicitudes-alojamiento/${idUsuario}`}
+                  className="nav-item"
+                >
+                  <FaHome className="nav-icon" />
+                  <span>Solicitudes</span>
+                </Link>
+              </>
+            )}
+          </nav>
 
-        {/* DERECHA: nombre + botón */}
-        <div className="header-right">
-          <span className="user-name">{nombre}</span>
-          <button className="logout-button" onClick={handleLogout}>
-            <FaSignOutAlt className="logout-icon" />
-            Cerrar Sesión
-          </button>
+          {/* DERECHA: nombre + botón */}
+          <div className="header-right">
+            <span className="user-name">{nombre}</span>
+            <button className="logout-button" onClick={handleLogout}>
+              <FaSignOutAlt className="logout-icon" />
+              Cerrar Sesión
+            </button>
+          </div>
         </div>
       </header>
     </>
